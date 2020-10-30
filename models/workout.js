d5855059a743a8839bc7ddbcf600741024b45d09
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-29 22:12:02 -0400

    Working on the Workouts DB

--- models/workout.js
+++ models/workout.js
@@ -4,8 +4,13 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema(
   {
-    day: {},
-    exercises: [],
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [{
+      
+    }],
   },
   {}
 );
