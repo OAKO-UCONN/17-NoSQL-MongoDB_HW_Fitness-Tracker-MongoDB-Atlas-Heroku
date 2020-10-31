e7802ddf37d25a799787d6c787faa43c2a2f3e95
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-30 20:41:35 -0400

    Working on API routes.

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Workout = require('../models/workout.js');
+const path = require('path');
 
 // your routes here
 
@@ -12,5 +13,26 @@ router.get("/exercise", (req, res) => {
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
+
+//Workouts the user has completed.
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+//Adds workout to the DataBase when the user creates it.
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+      .then((data) => res.json(data))
+      .catch((err) => {
+        console.log("err", err);
+        res.json(err);
+      });
+  });
 
 module.exports = router;
